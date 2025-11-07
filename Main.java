b2d4433606c548833f0ed144a80cdd9a84cdba83
Laude44 <halidahmet44@gmail.com>
2025-11-07 14:23:12 +0300

    Lab 6 problems. Not Finished

--- Main.java
+++ Main.java
@@ -5,13 +5,60 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         Random random = new Random();
 
+        /*
+        my answers to the questions:
 
-        
+        int[] myArr = {4, 8, 15, 16, 23, 42};
+
+        a- the length of the array is       = 6,
+        b- value of the first element is    = 4
+        c- index is                         = 5
+        d- it is                            = 15
+        e- myArry[3]                        = 16
+        f- console will show the exeption
+         */
+
+        // Senario 0: question 2
+        int[] myArr = {4, 8, 15, 16, 23, 42};
+     /*a*/   System.out.println(myArr.length);
+     /*b*/   System.out.println(myArr[0]); // First elements index is 0
+     /*c*/   System.out.println(myArr.length-1);
+     /*d*/   System.out.println(myArr[2]);
+     /*e*/   System.out.println(myArr[3]);
+     /*f*/   //System.out.println(myArr[10]); /* We saw the ArrayIndexOutOfBoundException error. Program saying that the value is bigger than the length of the array*/
+        System.out.println();
+        System.out.println();
 
 
+        //problem 1
+    int[] studentID = new int[20];
+    for(int i=0;i<studentID.length;i++){
+        studentID[i]=i+1000;
+    }
 
-
+        System.out.println(arrayExpander(studentID,42).length); //Prints the new length of the array
 
 
     }
+
+        // Problem 1
+    public static int[] arrayExpander(int[] a, int size){
+        int[] b = new int[size];
+        for(int i =0;i<a.length;i++){
+            b[i]=a[i];
+        }
+    return b;
+    }
+        //Problem 2
+    public static int[] arrayShifter(int[] a){
+        int[] b = new int[a.length];
+        for(int i=0;i<a.length;i++){
+            if(i>a.length){
+
+            }
+            b[i]=a[i+1];
+        }
+    }
+
+
 }
